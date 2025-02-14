9064e353935dc7c1d1acdaaf06b70b6010c5c237
Bishal Bashyal <waglemanish97@gmail.com>
2025-02-14 00:55:13 -0500

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout'; // Adjust path if necessary
-import HomePage from './pages/Homepage';
-import BlogsPage from './pages/BlogsPage';
-import BlogPost from './pages/BlogPost';
-import { Link } from 'react-scroll';
+import Layout from '/src/components/Layout'; // Adjust path if necessary
+import HomePage from '/src/pages/Homepage';
+import BlogsPage from '/src/pages/BlogsPage';
+import BlogPost from '/src/pages/BlogPost';
+
 
 const App = () => {
   return (
