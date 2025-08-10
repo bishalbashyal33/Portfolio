fca3f26c7f958168eb9d6b116c32b479598d1f5f
Bishal Bashyal <bishalbash33@gmail.com>
2025-08-10 16:37:48 -0400

    updated about me section to the shorter version

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -42,18 +42,7 @@ const About = () => {
       </div>
       
       <p className="text-gray-700 mb-12 mx-auto">
-      Natural phenomena when modeled as events are inherently random and at first glance, it is non-intuitive to think about understanding patterns in such random phenomenon.
-      However, Central limit theorem tells us that an aggregation of large random events irrespective of their nature will lead to Gaussian distribution.
-      What is seemingly random at a glance, could soon be studied as following a specific pattern, while we as human beings are able to deduce insights from what seemed impossible to me a few years ago.
-      <br /><br />
-      I believe that we have problems, and there are patterns to exploit, whether through an <b>algorithmic approach in software engineering</b>, learning <b>kernel functions via Neural Networks</b>, trying to predict causality with <b>data science</b>, or leveraging noisy patterns with sensitive <b>risk assessment in Quant</b>.
-      We are always looking for patterns.
-      <br /><br />
-      Patterns are everywhere, so are problems, some more interesting and with more hideous patterns than others.
-      I’m more interested in discovering, leveraging and exploiting such patterns to solve challenging and interesting problems with data, AI and fundamental software Engineering.
-      If this way of thinking resonates with you/your team’s vision.<b> Feel Free to Reachout. </b> I'm currently open to solving interesting industry challenges starting with Summer 2026 Internships.
-      <br /><br />
-      I'm a former <b>Physics Olympiad Champion</b>, and have been fortunate enough to have solved complex problems for Millitary-Grade Tracking Systems, scaled applications on cloud, developed AI powered solution that integrated with LLMs, and sensor data, and have gathered numerous national and international awards along the way.
+      I’m a current MS CS student(Research Track) advised by Professor Dr. Shayok Chakraborty at <b>Florida State University</b>, Tallahassee Florida. I'm a former <b>Physics Olympiad Champion</b>, and have been fortunate enough to have had the opportunity to solve complex problems for <b>Millitary-Grade Tracking Systems</b>, scaled applications on cloud, developed AI powered solution that integrated with LLMs, and sensor data, and have gathered numerous national and international awards along the way. I'm currently open to solving interesting industry challenges in the domain of data(Quantitative), AI and fundamental software engineering starting with Summer 2026 Internships.
 </p>
 
       
