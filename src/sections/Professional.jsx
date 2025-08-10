3be31fd131dc9cd678be182284d3d0db2b28c3fe
Bishal Bashyal <bishalbashyal@bishalmbp.local>
2025-08-10 15:53:11 -0400

    updated professional experience

--- src/sections/Professional.jsx
+++ src/sections/Professional.jsx
@@ -16,98 +16,97 @@ const Professional = () => {
    "Florida State (FSU)": {
       logo: "https://upload.wikimedia.org/wikipedia/en/6/6b/Florida_State_University_seal.svg",
       techStack: [
-        "Python (TensorFlow, PyTorch)",
-        "AWS (EC2, S3, Lambda)",
-        "Docker, Kubernetes",
-        "MongoDB, PostgreSQL"
+        "Python (PyTorch)",
+        "Linux, Hypergator",
+        "LLM, VLM, Statistics, ML",
+        "Git, GitHub"
       ],
       contributions: [
-        "Developed machine learning algorithms for predictive analytics",
-        "Led the implementation of automated data pipeline system",
-        "Optimized model deployment process reducing inference time by 40%"
+        "Developed algorithms for NeuralNetwork Compression",
+        "Implemented Baselines for OnGoing Research Projects",
+        "Optimized Existing methods on ModelRanking with Pipeline extension improving performance by over 30%"
       ],
       achievements: [
-        "Successfully delivered 3 major ML projects ahead of schedule",
-        "Reduced cloud infrastructure costs by 30%",
-        "Awarded 'Best Technical Innovation' for Q2 2023"
+        "Succesfully delivered an efficient Top-k Subtree similarity search Algorithm as Capstone for Advanced Database Systems",
+        "Reduced Pipeline execution time on B200 GPUs by 30%",
+        "Devised an efficient Neural Network Compression algorithm using PCA"
       ],
       personalityUpgrades: [
-        "Enhanced leadership skills by managing a team of 4 developers",
-        "Improved client communication through regular technical presentations",
+        "Collaborating and Working with Teams on Complex Research Questions",
+        "Developed new ability to implement systems and algorithms by reading Research Papers",
         "Developed strong problem-solving skills in high-pressure situations"
       ]
     },
     "Katmatic Solutions": {
       logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR96HS5bW8rebdSKp4v4ud_z682QJKmgdy4RQ&s",
       techStack: [
-        "Python (TensorFlow, PyTorch)",
-        "AWS (EC2, S3, Lambda)",
-        "Docker, Kubernetes",
-        "MongoDB, PostgreSQL"
+        ".NET, Angular, Python (PyTorch), Langchain, scikit-learn",
+        "AWS (EC2, S3, Lambda), Azure CosmosDB",
+        "Docker, Azure Kubernetes(CI/CD), Microsoft IIS",
+        "MongoDB, PostgreSQL,Redis,MQTT,ElasticSearch,Kibana, FAISS vector store"
       ],
       contributions: [
-        "Developed machine learning algorithms for predictive analytics",
-        "Led the implementation of automated data pipeline system",
-        "Optimized model deployment process reducing inference time by 40%"
+        "Developed machine learning algorithms for predictive inventory analytics and RSSI patterns for passive tag tracking",
+        "Led the implementation of a Generic ETL Engine for Enterprise Data Integrations using Fluid Templating",
+        "Reduced False Positive signal received on tracking passive tags with RF-Antenaas by 15%"
       ],
       achievements: [
-        "Successfully delivered 3 major ML projects ahead of schedule",
+        "Successfully delivered 3 Enterprise projects ahead of schedule",
         "Reduced cloud infrastructure costs by 30%",
-        "Awarded 'Best Technical Innovation' for Q2 2023"
+        "Awarded Equity in the company for excellent contribution towards new product lineups and strategic initatives."
       ],
       personalityUpgrades: [
-        "Enhanced leadership skills by managing a team of 4 developers",
-        "Improved client communication through regular technical presentations",
-        "Developed strong problem-solving skills in high-pressure situations"
+        "Enhanced leadership skills by managing a team of 4 developers and conducting Hiring interviews for new interns.",
+        "Improved client communication through regular technical presentations on product features and architecture review.",
+        "Developed strong problem-solving skills in time-sensitive high-pressure situations"
       ]
     },
     
     "FuseMachines": {
       logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrn4MwotHet5UgoXAGQuFdz25AeN9a7r4Uog&s",
       techStack: [
-        "Python (spaCy, NLTK)",
-        "Node.js, Express",
-        "React Native",
-        "Redis, MySQL"
+        "Python,NLTK,BERT,Langchain,RAG,CNN",
+        "Reactjs",
       ],
       contributions: [
-        "Built AI-powered chatbot platform",
-        "Developed custom NLP models",
-        "Created automated testing framework"
+        "Built AI-powered automated esssay scoring system for Rubric Based Grading Tasks.",
+        "Developed custom Ensemble models with finetuned BERT and LLM Based Scoring",
+        "Built a Plant disease detection system with CNN and LVQ"
       ],
       achievements: [
-        "Chatbot achieved 85% customer satisfaction rate",
-        "Reduced customer service costs by 40%",
-        "Won 'Innovation Excellence' award"
+        "Essay Scoring Ensemble method achieved near state of the art score on Automated Essay Scoring system.",
+        "Conducted 20 Paper Recitation sessions as the Team Lead.",
+        "Built a webapp based on our evaluated method for Automated Essay Scoring and Plant Disease Detection."
       ],
       personalityUpgrades: [
-        "Mastered agile development practices",
-        "Improved project estimation skills",
-        "Developed mentoring capabilities"
+        "Improved the ability to think mathematically around common challenges and pitfalls in Machine Learning",
+        "Improved Teamwork, Paper Recitation and Ability to Build Research Questions from past researchwork.",
+        "Built presentation and communication skills while showcasing our Research work"
       ]
     },
     "Pulchowk Campus": {
       logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStukpbzZJVI7CZsdcUn37z6iJ1-oT8soZNsQ&s",
       techStack: [
-        "Flutter",
-        "Firebase",
-        "Google Cloud Platform",
-        "Analytics Tools"
+        "Python,FastAPI,LayoutLM,Pytorch",
+        "Reactjs, Tailwindcss",
+        "Mongodb,Firebase,Postgresql",
+        
       ],
       contributions: [
-        "Led development of sustainable energy monitoring app",
-        "Implemented IoT integration system",
-        "Created data visualization dashboard"
+        "Built a Web Information Extraction Engine for Structured Text Documents",
+        "First Author Publication on Information Extraction from Structured Text Document",
+        "Contributed by building a publication repository and OpenCourseware platform for the CS department"
       ],
       achievements: [
-        "Reached regional finals in competition",
-        "Secured $50K in seed funding",
-        "Featured in TechCrunch"
+        "Received Industry Funded Research Grant for AI Research",
+        "Instructed in National Tech Events on topics like Pandas, Numpy, Data Science, Git, Github",
+        "Won multiple awards during my undergraduate years"
       ],
       personalityUpgrades: [
-        "Developed entrepreneurial mindset",
-        "Enhanced public speaking skills",
-        "Strengthened networking abilities"
+        "Developed ability to justify, explain and build stories around concepts for Teaching others.",
+        "Enhanced public speaking skills, teamwork, and foundational skills on DeepLearning",
+        "Strengthened software engineering and Object Oriented Analysis and Design skills",
+        "Built an entrepreneurial mindset and leadership capabilities via HultPrize initaitives"
       ]
     }
   };
