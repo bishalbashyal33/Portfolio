08cccae1f90a92640739df7a0b56d957ccfa2c3d
Bishal Bashyal <bishalbash33@gmail.com>
2025-08-10 16:51:47 -0400

    changed footer text

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -42,10 +42,9 @@ const About = () => {
       </div>
       
       <p className="text-gray-700 mb-12 mx-auto">
-      I’m a current MS CS student(Research Track) advised by Professor Dr. Shayok Chakraborty at <b>Florida State University</b>, Tallahassee Florida. I'm a former <b>Physics Olympiad Champion</b>, and have been fortunate enough to have had the opportunity to solve complex problems for <b>Millitary-Grade Tracking Systems</b>, scaled applications on cloud, developed AI powered solution that integrated with LLMs, and sensor data, and have gathered numerous national and international awards along the way. I'm currently open to solving interesting industry challenges in the domain of data(Quantitative), AI and fundamental software engineering starting with Summer 2026 Internships.
+      I’m a current MS CS student(Research Track) working in interesting research challenges under the supervision of Professor Dr. Shayok Chakraborty at <b>Florida State University</b>, Tallahassee Florida. I'm a former <b>Physics Olympiad Champion</b>, and have been fortunate enough to have had the opportunity to solve complex problems for <b>Millitary-Grade Tracking Systems</b>, scaled applications on cloud, developed <b>AI powered solutions</b> that integrated with LLMs, and sensor data, and have gathered numerous national and international awards along the way. I'm currently open to solving interesting industry challenges in the domain of data(Quantitative), AI and fundamental software engineering starting with Summer 2026 Internships.
 </p>
 
-      
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {profiles.map((text) => (
           <Button key={text} className="text-gray-700 border-gray-300 hover:bg-gray-100 bg-white">{text}</Button>
@@ -54,6 +53,6 @@ const About = () => {
     </div>
   );
 };
-//academic
+//academic profile
 
 export default About;
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-full bottom-0 bg-[white] py-2 text-center">
-      <p className="text-[black] text-sm">Copyright All Rights Reserved</p>
+      <p className="text-[black] text-sm">Copyright © All Rights Reserved | 100% Human Written 🖤  </p>
     </footer>
   );
 };
