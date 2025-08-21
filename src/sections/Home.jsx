1836a32836fcead549f57acf4c9130944b75ccb2
bishalbashyal <waglemanish97@gmail.com>
2025-08-21 15:33:05 -0400

    updated resume to fix spelling mistakes

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
     Say Hi! 🛫
   </Button>
   <Button 
-    onClick={() => window.open("https://drive.google.com/file/d/1HaxT5D4sEX3ag9XvBMpWyeI9YQvy-zoE/view?usp=sharing", "_blank")}
+    onClick={() => window.open("https://drive.google.com/file/d/145SqCw4An4PDyNWT2BW8rZSSI-Ef2CCN/view?usp=sharing", "_blank")}
   >
     Resume 📃
   </Button>
